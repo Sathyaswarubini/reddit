9ad9db0801802f7500aa6eddf81d894d79aa437f
Sathyaswarubini <104678076+Sathyaswarubini@users.noreply.github.com>
2024-12-15 02:57:24 +0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "reddit-app-s3" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "EKS/Eks-terraform/.terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
